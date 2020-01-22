421d6230d528ca2a825139b669f13fc3915f100f
Baghi <qbaghi@gs66-azathoth.gsfc.nasa.gov>
2020-01-21 22:02:34 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='pywer',
+    version='1.0.0',
+    description='A power spectral density estimation tool.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/qbaghi/pywer',
+    author='Quentin Baghi',
+    author_email='quentin.baghi@protonmail.com',
+    classifiers=[  # Optional
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: GNU License',
+
+        # Python versions
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keywords='spectral analysis',
+    packages=find_packages(),
+    python_requires='>=3.5', install_requires=['pyfftw', 'scipy', 'numpy']
+
+)
